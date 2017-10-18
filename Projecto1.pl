77f84c88f88369c0660867f00b59ee461dc819c3
Rafael Vieira (1141233) <1141233@isep.ipp.pt>
2017-10-18 22:27:28 +0000

    Projecto1.pl edited online with Bitbucket

--- Projecto1.pl
+++ Projecto1.pl
@@ -1,22 +1,26 @@
+%-utils--------------
+
+connected2(A, B) :- fronteira(A, B) ; fronteira(B, A).
+
 %-1------------------
 
-import :- import('paises.txt').
-import(TXT) :- [TXT].
+import :- import_file('paises.txt').
+import_file(TXT) :- [TXT].
 
 %-2------------------
 
 lista(C):-
     write('Continente: '), write(C), nl,
-    pais(P,C,H), write(P), write(, ), write(H), write(, ),
-    paisesFronteira(P,_,Lst), write(Lst).
+    pais(P, C, H), write(P), write(, ), write(H), write(, ),
+    paisesFronteira(P, _, Lst), write(Lst).
 
 paisesFronteira(P,X,Lst) :-
-    findall(X, fronteira(P,X); fronteira(X,P), Lst).
+    findall(X, connected2(P, X), Lst).
 
 %-3------------------
 
 doisMaisPop(P1, P2) :- 
-    findall(H, pais(_,_,H), LstH),
+    findall(H, pais(_, _, H), LstH),
     sort(0, @>=, LstH,  SortedH),
     doisMaisPop(P1, P2, SortedH).
 
@@ -27,38 +31,98 @@ doisMaisPop(P1, P2, [H1|[H2|_]]) :-
 %-4-----------------
 
 paisesGrandes(C, N, L) :- 
-    findall(H-P, (pais(P,C,H), H > N), L).
+    findall(H-P, (pais(P, C, H), H > N), L).
 
 %-5-----------------
 
 somaPopViz(P, L, S) :-
-    findall((H, P2), ((fronteira(P, P2); fronteira(P2, P)), pais(P2,_,H)), L), write(L),
+    findall((H, P2), (connected2(P, P2), pais(P2, _, H)), L), write(L),
     somaVizinhos(L, S).
 
-somaVizinhos([],0).
-somaVizinhos([(N,_)|L],S):-
-    somaVizinhos(L,S1),
+somaVizinhos([], 0).
+somaVizinhos([(N,_)|L], S):-
+    somaVizinhos(L, S1),
     S is S1 + N.
 
 %-6------------------
 
-numPaisesAtravessados(P1,P2,Num):- bfs(P1,P2,Cam), 
-    length(Cam,X),
-    Num is X-1.
+numPaisesAtravessados(P1, P2, Num):- bfs(P1, P2, Cam), 
+    length(Cam, X),
+    Num is X - 1.
 
-bfs(Orig,Dest,Cam):- bfs2(Dest, [[Orig]], Cam).
+bfs(Orig, Dest, Cam):- bfs2(Dest, [[Orig]], Cam).
 
-bfs2(Dest,[[Dest|T]|_], Cam):- reverse([Dest|T], Cam).
+bfs2(Dest, [[Dest|T]|_], Cam):- reverse([Dest|T], Cam).
 
-bfs2(Dest,[LA|Outros],Cam):-
+bfs2(Dest, [LA|Outros], Cam):-
     LA=[Act|_],
     findall([X|LA], (Dest\==Act, 
-        (fronteira(Act,X);fronteira(X,Act)), 
-        \+ member(X,LA)),
+        (connected2(Act, X)), 
+        \+ member(X, LA)),
         Novos),
-    append(Outros,Novos,Todos),
-    bfs2(Dest,Todos,Cam).
+    append(Outros, Novos, Todos),
+    bfs2(Dest, Todos, Cam).
 
+%-7------------------
+
+route(Orig, Dest, NFront, Cam):- 
+    calcRoute(Orig, Dest, NFront, Cam),
+    NFront1 is NFront + 1,
+    length(Cam, NFront1).
+
+calcRoute(Orig, Dest, NFront, Cam):- 
+    calcRoute2(Dest, [[Orig]], NFront, Cam,_).
+
+calcRoute2(Dest, [[Dest|T]|_], _, Cam, _):- 
+    reverse([Dest|T], Cam).
+
+calcRoute2(Dest, [LA|Outros], NFront, Cam, Num):-
+    LA = [Act|_],
+    findall([X|LA],
+        (Dest\==Act, connected2(Act, X),
+        \+ member(X, LA), length(LA, N),
+        N =< NFront, Num=N),
+        Novos),
+    append(Outros, Novos, Todos),
+    calcRoute2(Dest, Todos, NFront, Cam, Num).
+
+%-8------------------
+
+roteiroContinente(Cont, Orig, NFront, Cam):- 
+    pais(Orig, Cont, _), 
+    calculaRoteiros(Orig, _, NFront, Cam), 
+    NFront1 is NFront + 1, 
+    length(Cam, NFront1).
+
+calculaRoteiros(Orig, Dest, NFront, Cam):- 
+    calculaRoteiros2(Dest, [[Orig]], NFront, Cam).
+
+calculaRoteiros2(Dest, [[Dest|T]|_], _, Cam):- 
+    reverse([Dest|T], Cam).
+
+calculaRoteiros2(Dest, [LA|Outros], NFront, Cam):-
+    LA=[Act|_],
+    findall([X|LA],
+        (Dest\==Act, 
+        connected2(Act,X), 
+        \+ member(X,LA), 
+        length(LA,N), 
+        N =< NFront),
+        Novos),
+    append(Outros, Novos, Todos),
+    calculaRoteiros2(Dest, Todos, NFront, Cam).
+    
+%-9------------------
+
+%TODO
+
+%-10-----------------
+
+checkCores(R):- 
+    findall((P, C, L), (cor(C, P), listaCorViz(P, L)), R).
+
+listaCorViz(P, L):- 
+    findall(C, (connected2(P,P2), cor(C, P2)), L).
 
 %-11-----------------
 
@@ -89,4 +153,3 @@ exportar(F) :-
 
    close(OS),
    write('Done!').
-
