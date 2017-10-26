7332942f2ec9307af57454455a3249af1a55d464
Rafael Vieira (1141233) <1141233@isep.ipp.pt>
2017-10-26 10:03:35 +0000

    algav_tp2_tsp.pl edited online with Bitbucket

--- algav_tp2_tsp.pl
+++ algav_tp2_tsp.pl
@@ -57,6 +57,11 @@ city(prague,50.0878114,14.4204598).
 %city(kiev,50.440951,30.5271814).
 %city(cardiff,51.4813069,-3.1804979).
 
+
+
+% UTILS
+% 
+% 
 %  dist_cities(brussels,prague,D).
 %  D = 716837.
 dist_cities(C1,C2,Dist):-
@@ -84,6 +89,75 @@ distance(Lat1, Lon1, Lat2, Lon2, Dis2):-
 % distance(50.8462807,4.3547273,50.0878114,14.4204598,D).
 % Online: http://www.movable-type.co.uk/scripts/latlong.html
 %
+
+ 
+% Given three colinear points p, q, r, the function checks if
+% point q lies on line segment 'pr'
+%onSegment(P, Q, R)
+onSegment((PX,PY), (QX,QY), (RX,RY)):-
+    QX =< max(PX,RX),
+    QX >= min(PX,RX),
+    QY =< max(PY,RY),
+    QY >= min(PY,RY).
+
+ 
+% To find orientation of ordered triplet (p, q, r).
+% The function returns following values
+% 0 --> p, q and r are colinear
+% 1 --> Clockwise
+% 2 --> Counterclockwise
+
+orientation((PX,PY), (QX,QY), (RX,RY), Orientation):-
+	Val is (QY - PY) * (RX - QX) - (QX - PX) * (RY - QY),
+	(
+		Val == 0, !, Orientation is 0;
+		Val >0, !, Orientation is 1;
+		Orientation is 2
+	).
+ 
+orientation4cases(P1,Q1,P2,Q2,O1,O2,O3,O4):-
+    orientation(P1, Q1, P2,O1),
+    orientation(P1, Q1, Q2,O2),
+    orientation(P2, Q2, P1,O3),
+    orientation(P2, Q2, Q1,O4).
+ 
+
+% The main function that returns true if line segment 'p1q1'
+% and 'p2q2' intersect.
+doIntersect(P1,Q1,P2,Q2):-
+    % Find the four orientations needed for general and
+    % special cases
+	orientation4cases(P1,Q1,P2,Q2,O1,O2,O3,O4),
+	
+	(	
+    % General case
+    O1 \== O2 , O3 \== O4,!;
+
+    % Special Cases
+    % p1, q1 and p2 are colinear and p2 lies on segment p1q1
+    O1 == 0, onSegment(P1, P2, Q1),!;
+ 
+    % p1, q1 and p2 are colinear and q2 lies on segment p1q1
+    O2 == 0, onSegment(P1, Q2, Q1),!;
+ 
+    % p2, q2 and p1 are colinear and p1 lies on segment p2q2
+    O3 == 0, onSegment(P2, P1, Q2),!;
+ 
+     % p2, q2 and q1 are colinear and q1 lies on segment p2q2
+    O4 == 0, onSegment(P2, Q1, Q2),!
+    ).    
+    
+linearCoord(City,X,Y):-
+	city(City,Lat,Lon),
+	geo2linear(Lat,Lon,X,Y).
+
+geo2linear(Lat,Lon,X,Y):-
+	degrees2radians(Lat,LatR),
+	degrees2radians(Lon,LonR),
+	X is round(6371*cos(LatR)*cos(LonR)),
+	Y is round(6371*cos(LatR)*sin(LonR)).
+
+
 
 %Limit 10 Cities
 tsp1(City, Visited, Distance) :-
@@ -116,3 +190,42 @@ get_road(Start, End, Waypoints, DistanceAcc, Visited, TotalDistance) :-
     \+ member(Waypoint, Waypoints),
     NewDistanceAcc is DistanceAcc + Distance,
     get_road(Waypoint, End, [Waypoint|Waypoints], NewDistanceAcc, Visited, TotalDistance).
+
+
+
+tsp2(Orig, Visit):-
+    bestfs(Orig, Orig, Visit).    
+
+estimativa(X,Dest,EstX) :-
+    dist_cities(X, Dest, EstX).
+
+bestfs(Orig,Dest,Cam):-
+	bestfs2(Dest,[Orig],Cam).
+
+%condicao final: destino = nó à cabeça do caminho actual
+bestfs2(Dest,[Dest|T],Cam):- !,
+	%caminho actual está invertido
+	reverse([Dest|T],Cam).
+
+bestfs2(Dest,LA,Cam):-
+	LA=[Act|_],
+	%calcular todos os nodos adjacentes nao visitados e
+	% guardar um tuplo com estimativa e novo caminho
+	findall((EstX,[X|LA]),
+	(\+ member(X,LA), estimativa(X,Dest,EstX)),Novos),
+	%ordenar pela estimativa
+	sort(Novos,NovosOrd),
+	%extrair o melhor que está à cabeça
+	NovosOrd = [(_,Melhor)|_],
+	%chamada recursiva
+	bestfs2(Dest,Melhor,Cam).
+
+tsp3(Orig, Visit):-
+    tsp2(Orig, VisitTmp),
+    del_cross(TmpPath, Path).
+
+del_cross([H|T], Path) :- 
+    linearCoord(H, Hx, Hy),
+    doIntersect((Hx, Hy), ) ->
+  ; .
+    
